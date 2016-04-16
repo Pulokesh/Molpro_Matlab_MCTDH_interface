c1486aad9efddf544a1da43b8b1cf0bfc0509ddf
Ekadashi <ekadashipradhan@gmail.com>
2016-04-16 02:02:49 -0400

    updated

--- run_Molpro/extract1.py
+++ run_Molpro/extract1.py
@@ -2,8 +2,6 @@
 #* Copyright (C) 2016 Ekadashi Pradhan
 #*
 #******************************************************
-#from numpy.oldnumeric import *
-#from numpy.oldnumeric.linear_algebra import *
 import numpy
 from numpy import *
 
@@ -40,36 +38,11 @@ for line in output:
                  gradVector[5] = float(temp[5])
                  gradVector[6] = float(temp[6])
                  gradVector[7] = float(temp[7])
-#               gradVector[1] = float(temp[5])
-#               gradVector[2] = float(temp[8])
 
                  energyFile.write(str(gradVector[0])+" "+str(gradVector[1])+" "+str(gradVector[2])+" "
                   +str(gradVector[3])+" "+str(gradVector[4])+" "+str(gradVector[5])+" "+str(gradVector[6])
                   +" "+str(gradVector[7])+'\n')
-#                 if((temp[0]=="SETTING") and (temp[4] == "AU")):
-#                        foundLine = 1
-#                        continue # go to next iteration
-#	if(foundLine == 2):
-#		print 'okay'
-#		temp = line.split()
-#	 	if(len(temp) == 5):
-#		 print 'okay'
-#		gradVector[0] = float(temp[3])
-#		gradVector[1] = float(temp[5])
-#		gradVector[2] = float(temp[8])
-		
-#                dipoleFile.write(str(gradVector[0]) + "\n")
-#                dipoleFile.write(str(gradVector[0]) + " " + str(gradVector[1]) + " " + str(gradVector[2]) + "\n")
  	        foundLine =0
-
-#dipoleFile = open("dipoles", 'w')
-#dipoleFile = open("dipoles", 'a') # append
-#dipoleFile.write(str(gradVector[0]) + " " + str(gradVector[1]) + " " + str(gradVector[2]) + "\n")
-#dipoleFile = open("dipoles", 'a') # append
-
-
-
-
 
 
 output.close()
