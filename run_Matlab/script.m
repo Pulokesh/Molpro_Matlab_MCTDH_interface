83a4f1fadf3684f8de2efea5c775c30a9cb121e5
Ekadashi <ekadashipradhan@gmail.com>
2017-07-31 15:11:07 -0400

    updated script.m

--- run_Matlab/script.m
+++ run_Matlab/script.m
@@ -2,16 +2,19 @@
 %        Copyright (C) 2017 Ekadashi Pradhan
 %-----------------------------------------------------------
 %
-%% this is for batch job
-%% Create 80N folder in the same directory
-% loads train test, val set
+%	This script is for batch job submission from matlab command window
+%	or commandline.
+%	Create 80N folder in the same directory.
+%	Loads train test, and val set - these three files should be placed in the main directory
 	load('train');
 	load('test');
 	load('val');
 	%loading;
-	% Fitting with 80NN
+
+%	Fitting
 	HFCO=scaled_onestg(['HFCO'],6,train,test,val,80,0,0.0000000001,0,300,0);
-	% saving weights, biases, and errofiles in to the 80N folder
+
+%	Saving fitting parameters - weights, biases, and errofiles in to the 80N folder
 	copyfile ('HFCO.LW','80N/');
 	copyfile ('HFCO.IW','80N/');
 	copyfile ('HFCO.b','80N/');
@@ -20,6 +23,6 @@
 	copyfile ('HFCO.errorfile1','80N/');
 	clear
 
-%% run this script from commandline as
-% matlab -nodisplay <script.m> nohup.out &
-% performane can be checked from nohup.out while program is running.
+%	run this script from commandline as
+%	matlab -nodisplay <script.m> nohup.out &
+%	performane can be checked from nohup.out while program is running.
